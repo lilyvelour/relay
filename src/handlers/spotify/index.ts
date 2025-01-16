987a83ab85618d07658b15793c6b0e2b7d8e92aa
Matt Elphick <matt.a.elphy@gmail.com>
2025-01-16 23:18:36 +0000

    fix: Add error handling in spotify handlers

--- src/handlers/spotify/index.ts
+++ src/handlers/spotify/index.ts
@@ -3,28 +3,52 @@ import spotifyClient from './client'
 
 const handlers: EventHandler<any> = {
   play: async ({ msg, logger }) => {
-    const result = await spotifyClient.pause(msg)
-    logger.info('play', { ok: result.ok })
+    try {
+      const result = await spotifyClient.pause(msg)
+      logger.info('play', { ok: result.ok })
+    } catch (e) {
+      logger.error('play', { error: e.message })
+    }
   },
   pause: async ({ msg, logger }) => {
-    const result = await spotifyClient.pause(msg)
-    logger.info('pause', { ok: result.ok })
+    try {
+      const result = await spotifyClient.pause(msg)
+      logger.info('pause', { ok: result.ok })
+    } catch (e) {
+      logger.error('pause', { error: e.message })
+    }
   },
   next: async ({ msg, logger }) => {
-    const result = await spotifyClient.next(msg)
-    logger.info('next', { ok: result.ok })
+    try {
+      const result = await spotifyClient.next(msg)
+      logger.info('next', { ok: result.ok })
+    } catch (e) {
+      logger.error('next', { error: e.message })
+    }
   },
   previous: async ({ msg, logger }) => {
-    const result = await spotifyClient.previous(msg)
-    logger.info('previous', { ok: result.ok })
+    try {
+      const result = await spotifyClient.previous(msg)
+      logger.info('previous', { ok: result.ok })
+    } catch (e) {
+      logger.error('previous', { error: e.message })
+    }
   },
   info: async ({ logger }) => {
-    const result = await spotifyClient.info()
-    logger.info('info', { ok: result.ok })
+    try {
+      const result = await spotifyClient.info()
+      logger.info('info', { ok: result.ok })
+    } catch (e) {
+      logger.error('info', { error: e.message })
+    }
   },
   playing: async ({ logger }) => {
-    const result = await spotifyClient.playing()
-    logger.info('playing', { ok: result.ok })
+    try {
+      const result = await spotifyClient.playing()
+      logger.info('playing', { ok: result.ok })
+    } catch (e) {
+      logger.error('playing', { error: e.message })
+    }
   },
 }
 
